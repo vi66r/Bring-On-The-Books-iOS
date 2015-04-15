a1348da8e34b7fe4320c2ab790bcd1f367500d80
Rafi Rizwan <vi66r@Rafis-MacBook-Pro.local>
2015-04-15 17:11:19 -0400

    Initial Commit

---
+++ Bring On The Books/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bring On The Books
+//
+//  Created by Rafi Rizwan on 4/15/15.
+//  Copyright (c) 2015 Bring On The Books. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bring On The Books/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bring On The Books
+//
+//  Created by Rafi Rizwan on 4/15/15.
+//  Copyright (c) 2015 Bring On The Books. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
