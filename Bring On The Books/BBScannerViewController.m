c077942d95bf8b443077f42020af6dc7ed04fe00
Rafi Rizwan <vi66r@172-16-199-23.DYNAPOOL.NYU.EDU>
2015-04-15 18:25:52 -0400

    noman is super weird sometimes... ._____.

---
+++ Bring On The Books/BBScannerViewController.m
@@ -0,0 +1,38 @@
+//
+//  BBScannerViewController.m
+//  Bring On The Books
+//
+//  Created by Rafi Rizwan on 4/15/15.
+//  Copyright (c) 2015 Bring On The Books. All rights reserved.
+//
+
+#import "BBScannerViewController.h"
+#import <MTBBarcodeScanner/MTBBarcodeScanner.h>
+
+@interface BBScannerViewController ()
+
+@end
+
+@implementation BBScannerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Bring On The Books/BBUtilityConstants.h
@@ -0,0 +1,13 @@
+//
+//  BBUtilityConstants.h
+//  Bring On The Books
+//
+//  Created by Rafi Rizwan on 4/15/15.
+//  Copyright (c) 2015 Bring On The Books. All rights reserved.
+//
+
+#ifndef Bring_On_The_Books_BBUtilityConstants_h
+#define Bring_On_The_Books_BBUtilityConstants_h
+
+
+#endif
